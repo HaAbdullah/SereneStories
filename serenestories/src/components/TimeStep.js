4307e5991de34922b38901228fa1b42e3b2ebaa9
HaAbdullah <abdullah.hasanjee@gmail.com>
2024-01-27 16:16:22 -0500

    CSS Applied

--- serenestories/src/components/TimeStep.js
+++ serenestories/src/components/TimeStep.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './TimeStep.css'; // Import the CSS file
 
 const TimeStep = ({ onNext }) => {
-  const timeValues = ['15 minutes', '30 minutes', '1 hour']; // You can adjust these values
+  const timeValues = ['15 minutes', '30 minutes', '1 hour'];
 
   return (
-    <>
+    <div className="TimeStepContainer"> {/* Add a container div with the specified class */}
       <p>How much time do you have?</p>
       {timeValues.map((value) => (
         <button key={value} onClick={() => onNext(value)}>
           {value}
         </button>
       ))}
-    </>
+    </div>
   );
 }
 
